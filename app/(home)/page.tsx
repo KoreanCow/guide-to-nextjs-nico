61f5c13a8e7065d628effbe5f6ac23c1baabc374
KoreanCow <vue-cli@kakao.com>
2024-02-25 17:03:03 +0900

    Refactor: Fixed error in (home)/page.tsx in metadata

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,14 @@
 import styles from '../../styles/homepage.module.css'
 import Movie from '../../components/movie';
 
-export const metaData = {
-  title: 'Home',
-}
+export const metadata = {
+  title: "Home",
+};
 
-export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  // await new Promise((resolve) => setTimeout(resolve, 1000))
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(API_URL);
   const json = await response.json();
   return json;
@@ -16,16 +16,16 @@ async function getMovies() {
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return (<div className={styles.container}>
-    {movies.map(
-      (movie) => (
+  return (
+    <div className={styles.container}>
+      {movies.map((movie) => (
         <Movie
           key={movie.id}
           id={movie.id}
           poster_path={movie.poster_path}
-          title={movie.title} />
-      )
-    )};
-  </div>
-  )
+          title={movie.title}
+        />
+      ))}
+    </div>
+  );
 }
